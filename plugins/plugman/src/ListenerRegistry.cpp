a03d4271d6533c0f4b87f6b7afce2cce6e0f7d69
Rúben Carneiro <rubencarneiro01@gmail.com>
2022-12-14 09:28:25 +0000

    # Fix ListenerRegistry

--- plugins/plugman/src/ListenerRegistry.cpp
+++ plugins/plugman/src/ListenerRegistry.cpp
@@ -50,9 +50,9 @@ QQuickItem *ListenerRegistry::createListenerFromURl(const QString &url)
 
 void ListenerRegistry::componentComplete()
 {
-    auto plugins = PluginRegistry::instance()->getByLocation(QStringLiteral("Dekko::Listener"));
+    auto plugins = PluginRegistry::instance()->getByLocation(QStringLiteral("SageteaMail::Listener"));
     for (auto plugin : plugins) {
-        if (auto dp = qobject_cast<DekkoPlugin *>(plugin)) {
+        if (auto dp = qobject_cast<SageteaMailPlugin *>(plugin)) {
             qCDebug(PLUGIN_LISTENER) << "Loading plugin: " << dp->pluginId();
             QQuickItem *item = createListenerFromURl(dp->component());
             if (item) {
