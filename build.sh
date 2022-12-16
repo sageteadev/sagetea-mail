ff9217bc5aec835be8f6a91e2724d7f997892ce5
Rúben Carneiro <rubencarneiro01@gmail.com>
2022-12-16 14:55:54 +0000

    # Update config profiles

--- build.sh
+++ build.sh
@@ -46,9 +46,9 @@ else
     qbs setup-toolchains /usr/bin/${ARCH_TRIPLET}-gcc gcc-$ARCH
 fi
 
-qbs setup-qt /usr/lib/${ARCH_TRIPLET}/qt5/bin/qmake sageteamilqt5-$ARCH
-qbs config profiles.sageteamilqt5-$ARCH.baseProfile gcc-$ARCH
-qbs config profiles.sagetemailqt5-$ARCH.Qt.core.binPath /usr/lib/x86_64-linux-gnu/qt5/bin
+qbs setup-qt /usr/lib/${ARCH_TRIPLET}/qt5/bin/qmake sageteamailqt5-$ARCH
+qbs config profiles.sageteamailqt5-$ARCH.baseProfile gcc-$ARCH
+qbs config profiles.sageteamailqt5-$ARCH.Qt.core.binPath /usr/lib/x86_64-linux-gnu/qt5/bin
 
 export DIR_PREFIX=/lib/${ARCH_TRIPLET}
 export BIN_DIR=$DIR_PREFIX/bin
@@ -56,7 +56,7 @@ export LIB_DIR=$DIR_PREFIX
 export DATA_DIR=/usr/share/sageteamail
 export QML_DIR=$LIB_DIR
 
-qbs build -d $BUILD_DIR -f . --clean-install-root --show-progress config:build project.click:true project.pyotherside:false project.binDir:$BIN_DIR project.libDir:$LIB_DIR project.qmlDir:$QML_DIR project.dataDir:$DATA_DIR profile:dekkoqt5-$ARCH qbs.buildVariant:$BUILD_VARIANT qbs.installPrefix:/
+qbs build -d $BUILD_DIR -f . --clean-install-root --show-progress config:build project.click:true project.pyotherside:false project.binDir:$BIN_DIR project.libDir:$LIB_DIR project.qmlDir:$QML_DIR project.dataDir:$DATA_DIR profile:sageteamailqt5-$ARCH qbs.buildVariant:$BUILD_VARIANT qbs.installPrefix:/
 
 install_python_deps
 
